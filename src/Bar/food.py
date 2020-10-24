cfee51483c55d71def6800c3287578f7a1e80ee3
Rob <rob@Robs-iMac.local>
2020-10-24 18:42:03 +0100

    Added food and food_tests

--- src/Bar/food.py
+++ src/Bar/food.py
@@ -1,2 +1,6 @@
 class Food:
-    pass
+    def __init__(self, name, price, skill_required, ability_required):
+        self.ABILITY_REQUIRED = ability_required
+        self.SKILL_REQUIRED = skill_required
+        self.price = price
+        self.name = name
--- tests/food_test.py
+++ tests/food_test.py
@@ -1,6 +1,21 @@
 import unittest
 from src.Bar.food import *
+from src.food_ability import *
 
 
 class TestFood(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.food = Food("Soup", 399, 2, None)
+        self.food = Food("Soup", 399, 2, None)
+
+    def test_food_has_name(self):
+        self.assertEqual("Soup", self.food.name)
+
+    def test_food_has_price(self):
+        self.assertEqual(399, self.food.price)
+
+    def test_food_has_skill_required(self):
+        self.assertEqual(2, self.food.SKILL_REQUIRED)
+
+    def test_food_huas_ability_required(self):
+        self.assertEqual(None, self.food.ABILITY_REQUIRED)
